727faf66bdca0d8cd1b839b43a15fb1b548b4ae8
bb_domgarcia <dom.garcia@nuworks.ph>
2017-05-04 20:26:08 +0800

    separate app module

--- app/main.js
+++ app/main.js
@@ -1,22 +1,6 @@
 (function(app){
-    var NgModule = ng.core.NgModule;
-    var BrowserModule = ng.platformBrowser.BrowserModule;
     var platformBrowserDynamic = ng.platformBrowserDynamic.platformBrowserDynamic;
+    var AppModule = app.AppModule;
     
-    var QuoteService = app.QuoteService;
-    var RandomQuoteComponent = app.RandomQuoteComponent;
-    var AppComponent = app.AppComponent;
-
-    var AppModule = NgModule({
-        imports: [BrowserModule],
-        declarations: [AppComponent, RandomQuoteComponent],
-        providers: [QuoteService],
-        bootstrap: [AppComponent]
-    })
-    .Class({
-        constructor: function() {}
-    });
-
-   platformBrowserDynamic().bootstrapModule(AppModule);
-
+    platformBrowserDynamic().bootstrapModule(AppModule);
 })(window.app || (window.app = {}));
